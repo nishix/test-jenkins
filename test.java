37d082ee6b65a9e1c76979ca9bfa079c6228cc28
nishix <46733164+nishix@users.noreply.github.com>
2021-07-17 19:14:55 +0900

    Create test.java

---
+++ test.java
@@ -0,0 +1,43 @@
+
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+import java.io.IOException;
+import java.io.PrintWriter;
+ 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+ 
+@WebServlet("/HelloWorld")//[1]
+public class HelloWorld extends HttpServlet {//[2]
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {//[3]
+        response.setContentType("text/html");//[4]
+                       PrintWriter out = response.getWriter();//[5]
+                       out.println("<html><head></head><body>");//[6]
+                       out.println("<p>Sample</p>");//[7]
+                       out.println("<p>Hello World!</p>");//[8]
+                       out.println("</body></html>");//[9]
+                   }
+}
